fc00e7db799cb25c5ccf247b722548ea8c419389
wilson <contato@wilsonsantos.net>
2022-09-25 18:04:19 -0300

    Add Model Word

---
+++ Termoo/src/Models/Word.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Termoo.src.Models
+{
+  public class Word
+  {
+    private string[] words = new string[10];
+
+    public string sortWordOfDay()
+    {
+      Random rnd = new Random();
+      int position = rnd.Next(0, 9);
+      getValidWords();
+      return this.words[position];
+    }
+
+    public bool isValidWord(string word) 
+        => this.words.Contains(word);
+
+    //Isso ficaria bonitão em sqlite
+    public void getValidWords()
+    {
+      this.words[0] = "AVIÃO";
+      this.words[1] = "PAVÃO";
+      this.words[2] = "AVIAR";
+      this.words[3] = "PAVIO";
+      this.words[4] = "ÁUDIO";
+      this.words[5] = "COISA";
+      this.words[6] = "VEIAS";
+      this.words[7] = "ESQUI";
+      this.words[8] = "ÁGUIA";
+      this.words[9] = "ESPIÃ";
+
+    }
+
+  }
+}
