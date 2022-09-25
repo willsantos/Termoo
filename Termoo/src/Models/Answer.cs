a0e1309656009233ed6428ec6123ba815a85a22b
wilson <contato@wilsonsantos.net>
2022-09-25 18:08:45 -0300

    Add Model Answer

---
+++ Termoo/src/Models/Answer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Termoo.src.Models
+{
+    public class Answer
+    {
+        public static string formatWord(string wordPlayed)
+            => wordPlayed.ToUpper();
+    }
+}
