c70861ad10f58a4867ddeb014b1b986bfb9db5c8
wilson <contato@wilsonsantos.net>
2022-09-25 16:43:48 -0300

    Style the texts

--- Termoo/Program.cs
+++ Termoo/Program.cs
@@ -1,4 +1,4 @@
-using Termoo;
+﻿using Termoo;
 
 Termo game = new Termo();
 game.sortWordOfDay();
@@ -13,7 +13,9 @@ Console.WriteLine("Encontre a palavra do dia! São apenas 5 letras");
 Console.WriteLine($"DEBUG: A PALAVRA ATUAL É {game.wordOfDay}");
 do
 {
-  Console.WriteLine($"Você ainda tem {5 - game.Attempts} tentativas");
+  Console.ForegroundColor = ConsoleColor.Magenta;
+  Console.WriteLine($"\u2665 Você ainda tem {5 - game.Attempts} tentativas");
+  Console.ResetColor();
   if (game.LettersUsed.Count() > 0)
   {
     Console.WriteLine("Letras já usadas");
@@ -36,22 +38,30 @@ do
   switch (WordResult)
   {
     case Status.WIN:
-      Console.WriteLine("Parabens");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("Parabens você acertou");
+      Console.ResetColor();
       break;
     case Status.UNKNOWN:
-      Console.WriteLine("Não conheço essa palavra");
+      Console.ForegroundColor = ConsoleColor.DarkMagenta;
+      Console.WriteLine("==>Não conheço essa palavra");
+      Console.ResetColor();
       break;
     case Status.GAMEOVER:
+      Console.ForegroundColor = ConsoleColor.Red;
       Console.WriteLine("Não tem chororo, esse jogo acabou!");
+      Console.ResetColor();
       break;
     case Status.WRONG:
+      Console.ForegroundColor = ConsoleColor.DarkYellow;
       Console.WriteLine("Acertou alguma coisa");
+      Console.ResetColor();
       break;
     default:
       throw new Exception();
   }
   if (WordResult != Status.WIN)
-  game.colorLetter();
+    game.colorLetter();
 } while (game.Attempts < 5 && WordResult != Status.WIN);
 
 
